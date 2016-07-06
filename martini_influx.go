fb1aeb67e6d2c065954b475762c56d4cb5d0117a
yangyi-bupt <yangyi86123@gmail.com>
2016-07-06 21:40:07 +0800

    Add un-tested influxdb reporter support

---
+++ martini_influx.go
@@ -0,0 +1,74 @@
+// Package martini is a powerful package for quickly writing modular web applications/services in Golang.
+//
+// For a full guide visit http://github.com/go-martini/martini
+//
+//  package main
+//
+//  import "github.com/go-martini/martini"
+//
+//  func main() {
+//    m := martini.Classic()
+//
+//    m.Get("/", func() string {
+//      return "Hello world!"
+//    })
+//
+//    m.Run()
+//  }
+package martini
+
+import (
+    // "log"
+    // "net"
+    // "net/http"
+    // "os"
+    // "reflect"
+
+    // "github.com/codegangsta/inject"
+    "github.com/influxdata/influxdb/client/v2"
+    "github.com/golang/glog"
+)
+
+const (
+    DB_NAME string = "ApiReport"
+    BATCH_SIZE int = 3
+
+)
+// needs to create database systemstats
+func ReportPoint(c client.Client, msgsQ chan *client.Point) {
+    for {
+        bp, err := client.NewBatchPoints(client.BatchPointsConfig{
+            Database:  DB_NAME,
+            Precision: "s",
+        })
+        if err != nil {
+            glog.Warning("Error: NewBatchPoints err=", err.Error())
+        }
+
+        for i := 0; i < BATCH_SIZE; i++ {
+            pt := <-msgsQ
+            glog.V(30).Info("Add Point...", pt)
+            bp.AddPoint(pt)
+        }
+
+        glog.V(30).Infof("Sending %d Point to influxdb...\n", BATCH_SIZE)
+        if err = c.Write(bp); err != nil {
+            glog.Warning("Error Write influxdb err=", err.Error())
+        }
+    }
+}
+
+// Add msgQ and for msg report
+func InfluxM(c client.Client) *ClassicMartini {
+    r := NewRouter()
+    m := New()
+    var msgQ = make(chan *client.Point)
+    m.Map(msgQ)
+    m.Use(InfluxLogger())
+    m.Use(Recovery())
+    m.Use(Static("public"))
+    m.MapTo(r, (*Routes)(nil))
+    m.Action(r.Handle)
+    go ReportPoint(c, msgQ)
+    return &ClassicMartini{m, r}
+}
